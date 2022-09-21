2f2efa16bb402906542bee6b1678208b05690504
NeginSal <neginsaljooghi@gmail.com>
2022-09-21 17:06:56 +0430

    [WIP]-Added CharacterList component & create firts query

--- src/pages/CharacterList.js
+++ src/pages/CharacterList.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useQuery } from '@apollo/client';
+
+const GET_CHARACTERS = gql`
+  query{
+    characters {
+      results{
+       id
+       name 
+       image
+      }
+    }
+  } 
+`;
 
 const CharacterList = () => {
-    return ( 
+    const {data,loading,error}= useQuery(GET_CHARACTERS)
+    console.log({data,loading,error})
+    return (
         <div>
-            
+
         </div>
-     );
+    );
 }
- 
+
 export default CharacterList;
